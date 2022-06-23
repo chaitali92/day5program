1e9e1e01b361ec78a098c2998c37bb9de6d85deb
chaitali92 <chaitupadmawar11@gmail.com>
2022-06-23 18:19:37 +0530

    this is java basic program

--- src/oddeven/swap.java
+++ src/oddeven/swap.java
@@ -6,7 +6,7 @@ public class swap {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int a,b;
+int a,b,c;
 Scanner sc = new Scanner(System.in);
 System.out.println("ENTER THE FIRST NYMBER=");
 a=sc.nextInt();
@@ -14,9 +14,9 @@ System.out.println("ENTER THE SECOND NYMBER=");
 b=sc.nextInt();
 System.out.println("BEFORE WE SWAPPING=");
 System.out.println("a="+a+",b="+b);
-a=a^b;
-b=a^b;
-a=a^b;
+c=a;
+a=b;
+b=c;
 System.out.println("AFTER SWAPPING=");
 
 System.out.println("a="+a+",b="+b);
